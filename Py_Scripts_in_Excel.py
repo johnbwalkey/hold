0661abf3d6af53f8beabc679d35b769d96708e4a
johnbwalkey <john@walkey.ca>
2024-07-18 16:27:07 -0600

    j

---
+++ Py_Scripts_in_Excel.py
@@ -0,0 +1,16 @@
+# NeuralNine - call Py scripts from Excel using VBA
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+data =pd.read_excel("mydata.xlsx")
+print (data)
+
+data.prices.hist()
+plt.show()
+
+grouped_data=data.grouppby(by="categories").mean()
+plt.bar(grouped_data.index, grouped_data.prices)
+plt.show()
+
+
