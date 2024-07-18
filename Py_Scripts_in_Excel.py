f11f8fef31d4e5b8877608eb282a5222d89c964f
johnbwalkey <john@walkey.ca>
2024-07-18 16:59:12 -0600

    l

--- Py_Scripts_in_Excel.py
+++ Py_Scripts_in_Excel.py
@@ -10,14 +10,16 @@ data.prices.hist()
 plt.show()
 
 data =pd.read_excel("mydata.xlsx")
+print(data)
 grouped_data = data.groupby(by="categories").mean()
 plt.bar(grouped_data.index, grouped_data.prices)
 plt.show()
 
 # comment out selected line by doing ctrl and /
 data =pd.read_excel("mydata.xlsx")
-One_category = pd.get_dummies(data.categories)
-data  = data.join(One_category)
+print(data)
+One_categories = pd.get_dummies(data.categories)
+data  = data.join(One_categories)
 data.drop(["categories"], axis=1)
 
 sns.heatmap(data.corr(), annot=True, cmap="YlGnBu")
