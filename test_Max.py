5f412b28f762ba4dee160315fa30477e1749a4f5
johnbwalkey <john@walkey.ca>
2024-07-23 10:02:57 -0600

    Merge remote-tracking branch 'origin/main'

--- test_Max.py
+++ test_Max.py
@@ -5,3 +5,10 @@ message ="World"
 print(dir(message))
 #Q: what does dir do here?
 #A:
+# In Python, the dir() function is used to list the names of the attributes and methods
+# of an object. When you pass an object to dir(), it returns a list of the attributes
+# and methods associated with that object.
+# This will output a list of the attributes and methods that belong to the string
+# object message. These include all the built-in methods available for strings,
+# such as upper(), lower(), strip(), and many others.
+
